a556bf28a4a434b827be9060ac832a71c52c9927
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2026-04-15 19:25:02 -0300

    feat: serializing new room modifiers

--- Assets/_Game/_Scripts/Space/SpaceCreator.cs
+++ Assets/_Game/_Scripts/Space/SpaceCreator.cs
@@ -15,6 +15,11 @@ namespace Backrooms
         [Header("Single Room")]
         [SerializeField] private int _roomMinWidth;
         [SerializeField] private int _roomMinLength;
+        [SerializeField, Range(0.0f, 0.3f)] private float _roomBottomLeftModifier;
+        [SerializeField, Range(0.7f, 1f)] private float _roomTopRightModifier;
+        [SerializeField, Range(0, 2)] private int _roomOffset;
+
+        [Header("Single Corridor")]
         [SerializeField] private int _corridorWidth;
 
         [Header("Creation")]
@@ -32,7 +37,7 @@ namespace Backrooms
 
         private void CreateSpace()
         {
-            List<Node> roomsList = _generator.CalculateRooms(_maxIterations, _roomMinWidth, _roomMinLength);
+            List<Node> roomsList = _generator.CalculateRooms(_maxIterations, _roomMinWidth, _roomMinLength, _roomBottomLeftModifier, _roomTopRightModifier, _roomOffset);
 
             foreach (Node room in roomsList)
                MeshSpawner.CreateMesh(room.BottomLeftAreaCorner, room.TopRightAreaCorner, "Mesh_Floor_", _floorMaterial);
--- Assets/_Game/_Scripts/Space/SpaceGenerator.cs
+++ Assets/_Game/_Scripts/Space/SpaceGenerator.cs
@@ -6,7 +6,6 @@ namespace Backrooms
     {
         private int _totalWidth;
         private int _totalLength;
-
         private List<RoomNode> _totalNodes = new();
 
         public SpaceGenerator(int totalWidth, int totalLength)
@@ -15,14 +14,14 @@ namespace Backrooms
             this._totalLength = totalLength;
         }
 
-        public List<Node> CalculateRooms(int maxIterations, int roomMinWidth, int roomMinLength)
+        public List<Node> CalculateRooms(int maxIterations, int roomMinWidth, int roomMinLength, float roomBottomLeftModifier, float roomTopRightModifier, int roomOffset)
         {
             BinarySpacePartitioner bsp = new BinarySpacePartitioner(_totalWidth, _totalLength);
             _totalNodes = bsp.PrepareNodesCollection(maxIterations, roomMinWidth, roomMinLength);
             
             List<Node> roomSpaces = StructureHelper.TraverseGraph(bsp.RootNode);
             RoomGenerator roomGenerator = new RoomGenerator(maxIterations, roomMinWidth, roomMinLength);
-            List<RoomNode> roomNodes  = roomGenerator.GenerateRoomsBySpaces(roomSpaces);
+            List<RoomNode> roomNodes  = roomGenerator.GenerateRoomsBySpaces(roomSpaces, roomBottomLeftModifier, roomTopRightModifier, roomOffset);
 
             return new List<Node>( roomNodes);
         }
