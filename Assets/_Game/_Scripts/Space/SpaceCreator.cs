b3073f27b249dab496726c601ecac3f0d726be02
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2026-04-15 19:11:02 -0300

    feat: MeshSpawner created

--- Assets/_Game/_Scripts/Space/SpaceCreator.cs
+++ Assets/_Game/_Scripts/Space/SpaceCreator.cs
@@ -11,7 +11,7 @@ namespace Backrooms
         [Header("Total Space")]
         [SerializeField] private int _totalWidth;
         [SerializeField] private int _totalLength;
-        
+
         [Header("Single Room")]
         [SerializeField] private int _roomMinWidth;
         [SerializeField] private int _roomMinLength;
@@ -20,22 +20,22 @@ namespace Backrooms
         [Header("Creation")]
         [SerializeField] private int _maxIterations;
 
+        [Header("References")]
+        [SerializeField] private Material _floorMaterial;
+
         // Not serialized
         private SpaceGenerator _generator;
 
-        private void Awake()
-        {
-            _generator = new SpaceGenerator(_totalWidth, _totalLength);
-        }
+        private void Awake() => _generator = new SpaceGenerator(_totalWidth, _totalLength);
 
-        private void Start()
-        {
-            CreateBackrooms();
-        }
+        private void Start() => CreateSpace();
 
-        private void CreateBackrooms()
+        private void CreateSpace()
         {
             List<Node> roomsList = _generator.CalculateRooms(_maxIterations, _roomMinWidth, _roomMinLength);
+
+            foreach (Node room in roomsList)
+               MeshSpawner.CreateMesh(room.BottomLeftAreaCorner, room.TopRightAreaCorner, "Mesh_Floor_", _floorMaterial);
         }
     }
 }
