83ed82278d244a078aadb967cf8f63a5369a3b3c
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2026-04-18 17:26:39 -0300

    feat: creating walls

--- Assets/_Game/_Scripts/Utilities/MeshSpawner.cs
+++ Assets/_Game/_Scripts/Utilities/MeshSpawner.cs
@@ -4,7 +4,8 @@ namespace Backrooms
 {
     public static class MeshSpawner
     {
-        public static void CreateMesh(Vector2 bottomLeftAreaCorner, Vector2 topRightAreaCorner, string prefixName, Material material)
+        public static void CreateMesh(Vector2 bottomLeftAreaCorner, Vector2 topRightAreaCorner, 
+                    string prefixName, Material material, GameObject wallParent)
         {
             Vector3 bottomLeftPoint = new Vector3(bottomLeftAreaCorner.x, 0, bottomLeftAreaCorner.y);
             Vector3 topRightPoint = new Vector3(topRightAreaCorner.x, 0, topRightAreaCorner.y);            
