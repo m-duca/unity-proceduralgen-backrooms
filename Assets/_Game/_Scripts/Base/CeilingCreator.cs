63e919775bad41be91b387c1ccf7d443cc44f4b2
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2026-04-19 18:37:01 -0300

    feat: CeilingCreator

--- Assets/_Game/_Scripts/Base/CeilingCreator.cs
+++ Assets/_Game/_Scripts/Base/CeilingCreator.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 namespace Backrooms
 {
-    public class FloorCreator
+    public class CeilingCreator
     {
-        public void CreateFloor(Vector2 bottomLeftAreaCorner, Vector2 topRightAreaCorner, Material material, Transform parent)
+        public void CreateCeiling(Vector2 bottomLeftAreaCorner, Vector2 topRightAreaCorner, float height, Material material, Transform parent)
         {
-            Vector3 bottomLeftPoint = new Vector3(bottomLeftAreaCorner.x, 0, bottomLeftAreaCorner.y);
-            Vector3 topRightPoint = new Vector3(topRightAreaCorner.x, 0, topRightAreaCorner.y);
-            Vector3 bottomRightPoint = new Vector3(topRightAreaCorner.x, 0, bottomLeftAreaCorner.y);
-            Vector3 topLeftPoint = new Vector3(bottomLeftAreaCorner.x, 0, topRightAreaCorner.y);
+            Vector3 bottomLeftPoint = new Vector3(bottomLeftAreaCorner.x, height, bottomLeftAreaCorner.y);
+            Vector3 topRightPoint = new Vector3(topRightAreaCorner.x, height, topRightAreaCorner.y);
+            Vector3 bottomRightPoint = new Vector3(topRightAreaCorner.x, height, bottomLeftAreaCorner.y);
+            Vector3 topLeftPoint = new Vector3(bottomLeftAreaCorner.x, height, topRightAreaCorner.y);
 
             Vector3[] vertices = new Vector3[]
             {
@@ -20,19 +20,18 @@ namespace Backrooms
             };
 
             Vector2[] uvs = new Vector2[vertices.Length];
+
             for (int i = 0; i < uvs.Length; i++)
-            {
                 uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
-            }
 
             int[] triangles = new int[]
             {
                 0, 
-                1, 
                 2, 
+                1,
                 2, 
-                1, 
-                3
+                3, 
+                1
             };
 
             Mesh mesh = new Mesh();
@@ -40,15 +39,12 @@ namespace Backrooms
             mesh.uv = uvs;
             mesh.triangles = triangles;
 
-            GameObject meshGo = new GameObject($"Mesh_Floor_{bottomLeftAreaCorner}", typeof(MeshFilter), typeof(MeshRenderer));
+            GameObject meshGo = new GameObject($"Mesh_Ceiling_{bottomLeftAreaCorner}", typeof(MeshFilter), typeof(MeshRenderer));
             
             if (parent != null)
                 meshGo.transform.parent = parent;
-
-            meshGo.transform.localPosition = Vector3.zero;
-            meshGo.transform.localScale = Vector3.one;
-            meshGo.transform.rotation = Quaternion.identity;
             
+            meshGo.transform.position = Vector3.zero;
             meshGo.GetComponent<MeshFilter>().mesh = mesh;
             meshGo.GetComponent<MeshRenderer>().material = material;
         }
